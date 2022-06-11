01361056fb443fdcf54aade7af5b789c1245b4c4
Wolfgang Reissenberger <sterne-jaeger@openfuture.de>
2022-06-11 20:49:18 +0200

    Arduino PowerBox V 1.1 | PowerBox INDI driver configuration separated from other Duino projects

---
+++ indi-duino/indi_duino_powerbox.xml.cmake
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<driversList>
+<devGroup group="Auxiliary">
+    <device label="Arduino Power Box" manufacturer="DIY">
+        <driver name="Arduino Power Box">indi_duino_powerbox</driver>
+        <version>@DUINOPOWERBOX_VERSION_MAJOR@.@DUINOPOWERBOX_VERSION_MINOR@</version>
+    </device>
+</devGroup>
+</driversList>
